240f0ffcccb57dfff116001e3fd99b08490c1c5b
FerreteriaRoma <fvirtualroma@gmail.com>
2024-10-21 21:20:15 -0500

    Eliminacion login con google

--- components/Layout.js
+++ components/Layout.js
@@ -5,19 +5,6 @@ import { useSession, signIn } from "next-auth/react";
 export default function Layout({ children }) {
   const { data: session } = useSession();
 
-  if (!session) {
-    return (
-      <div className="layout-container">
-        <div className="login-container">
-          <img src="/img/Logo.png" className="logo" alt="Logo" />
-          <button onClick={() => signIn('google')} className="login-button">
-            Login con Google
-          </button>
-        </div>
-      </div>
-    );
-  }
-
   return (
     <div className="bg-vinoT min-h-screen flex">
       <Nav />
